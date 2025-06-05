67c13e5b9c27477d5d9e5a8e7b392217c4d34a11
vagnersousan <vagnersousan@hotmail.com>
2025-06-05 02:30:02 -0300

    Ajustes

---
+++ src/sanityClient.js
@@ -0,0 +1,9 @@
+'''import {createClient} from '@sanity/client'
+
+export default createClient({
+  projectId: '8u8f8r8d', // find this at manage.sanity.io or in your sanity.json
+  dataset: 'production', // this is from those question during 'sanity init'
+  useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: '2024-06-05', // use a UTC date string
+})
+'''
