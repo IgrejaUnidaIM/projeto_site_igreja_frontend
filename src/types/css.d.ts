38825b6a8f6530f80004c9912a31a082752ab561
vagnersousan <vagnersousan@hotmail.com>
2025-06-20 16:48:59 -0300

    acerto no conteudo dos códigos 10

--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -1,2 +1,27 @@
 // src/types/css.d.ts
-declare module '*.css';
+
+declare module 'swiper/css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module 'swiper/css/navigation' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module 'swiper/css/pagination' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module 'swiper/css/autoplay' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+// Para qualquer outro arquivo .css genérico que você possa importar
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
